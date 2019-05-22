97dc15c5c3aa4a0e1b1df5eedbd87177e602bbe8
nazmulsharif <nazmulict94@gmail.com>
2019-05-22 15:51:00 +0600

    my portfolio

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/*prettyPhoto starts*/
+
+
+$('.gal a').prettyPhoto();
+
+/*prettyPhoto ends*/
+
+
+/*active class add*/
+$('.header li a').click(function(){
+      $('.header li a').removeClass('active');
+      $(this).addClass('active');
+    })
+/*active class add*/
+
+
+
+/*quote slider starts*/
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    }
+
+  /*  Quote slider ends*/
